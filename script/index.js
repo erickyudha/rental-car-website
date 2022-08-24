c704b01ee4e332ba8c7a98355c6f4ac0cb436278
Erick Yudha <erickyudha18@gmail.com>
2022-08-24 15:44:04 +0700

    finished navbar animation

--- script/index.js
+++ script/index.js
@@ -13,11 +13,58 @@ resetNavState()
 
 // --button event
 navToggleBtn.addEventListener("click", () => {
+    // Summon the element
     overlayLayer.style.display = "block"
     sidebarElement.style.display = "flex"
+
+    // Hide the element
+    overlayLayer.style.opacity = "0";
+    sidebarElement.style.right = "-50vw";
+
+    // Animate the element
+    let sidebarPos = -50;
+    let overlayOpacity = 0;
+
+    const inAnimation = () => {
+        if (sidebarPos >= 0 && overlayOpacity >= 100) {
+            clearInterval(timer);
+        } else {
+            sidebarElement.style.right = sidebarPos + "vw";
+            overlayLayer.style.opacity = "" + overlayOpacity;
+            sidebarPos += 2;
+            overlayOpacity += 4;
+        }
+    }
+    const timer = setInterval(inAnimation, 5);
 })
 
-exitSidebarBtn.addEventListener("click", resetNavState)
+exitSidebarBtn.addEventListener("click", () => {
+    // Animate the element
+    let sidebarPos = 0;
+    let overlayOpacity = 100;
+
+    const outAnimation = () => {
+        if (sidebarPos <= -50 && overlayOpacity <= 0) {
+            // Unsummon the element
+            resetNavState();
+
+            clearInterval(timer);
+        } else {
+            sidebarElement.style.right = sidebarPos + "vw";
+            overlayLayer.style.opacity = "" + overlayOpacity;
+            sidebarPos -= 2;
+            overlayOpacity -= 4;
+        }
+    }
+    const timer = setInterval(outAnimation, 5);
+})
+
+// -- reset when viewport resized
+window.onresize = () => {
+    if (window.innerWidth > 768) {
+        resetNavState()
+    }
+}
 
 
 //-----------------------------------------------------
@@ -36,7 +83,7 @@ $(document).ready(function () {
     });
 });
 
-// Scroll dengan penyesuaian tinggi navbar
+// Scroll offset with navbar
 let sidebarElements = document.getElementById("sidebar-marker").children;
 let topNavElements = document.getElementById("nav-marker").children;
 let footerNavElements =
