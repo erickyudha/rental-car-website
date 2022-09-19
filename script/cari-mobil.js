0bb9afe1a52a830aa0dccaaf49f0d5cc965080a4
Erick Yudha <erickyudha18@gmail.com>
2022-09-20 03:11:56 +0700

    besic select dropdown js done

--- script/cari-mobil.js
+++ script/cari-mobil.js
@@ -1,5 +1,8 @@
 const selectInputArr = document.getElementsByClassName("select-input");
+const selectBtnArr = document.getElementsByClassName("select-btn")
 const selectDropArr = document.getElementsByClassName("select-dropdown");
+
+const radioNameArr = ["driverType"]
 
 const changeWidthBatch = (elementArr, targetWidth) => {
     for (let element of elementArr) {
@@ -12,8 +15,61 @@ const changeDropWidth = () => {
     changeWidthBatch(selectDropArr, selectInputWidth);
 }
 
+const dropdownViewEvent = () => {
+    // UNHIDE DROPDOWN WHEN FORM BUTTON CLICKED
+    for (let i = 0; i < selectBtnArr.length; i++) {
+        selectBtnArr[i].addEventListener("click", () => {
+            const dropdown = selectDropArr[i];
+            if (dropdown.classList.contains("active")) {
+                dropdown.classList.remove("active");
+            } else {
+                dropdown.classList.add("active");
+            }
+        })
+    }
+
+    for (let i = 0; i < selectDropArr.length; i++) {
+        const element = selectDropArr[i].children;
+    }
+}
+
+const dropdownClickEvent = () => {
+    for (let i = 0; i < selectDropArr.length; i++) {
+        const list = selectDropArr[i].children;
+        for (let j = 0; j < list.length; j++) {
+            const listItem = list[j];
+            listItem.addEventListener("click", () => {
+                const input = listItem.children[0]
+                const text = listItem.children[1].innerHTML;
+                selectBtnArr[i].children[0].innerHTML = text;
+                input.checked = true;
+
+                selectDropArr[i].classList.remove("active");
+            })
+        }
+    }
+}
+
 window.addEventListener("resize", () => {
     changeDropWidth();
 })
 
-changeDropWidth();
+const start = () => {
+    changeDropWidth();
+    dropdownViewEvent();
+    dropdownClickEvent();
+}
+
+start()
+
+
+
+
+
+/* console.log(selectInputArr[0].children);
+let selectedInputNum = null;
+
+
+
+const radio1 = document.getElementsByName("driverType")
+console.log(radio1); */
