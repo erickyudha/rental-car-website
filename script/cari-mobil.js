6c163a7cccde4a66620845939272975bc7837b57
Erick Yudha <erickyudha18@gmail.com>
2022-09-20 16:32:25 +0700

    comp design and js for drop select and text input

--- script/cari-mobil.js
+++ script/cari-mobil.js
@@ -1,8 +1,7 @@
 const selectInputArr = document.getElementsByClassName("select-input");
 const selectBtnArr = document.getElementsByClassName("select-btn")
 const selectDropArr = document.getElementsByClassName("select-dropdown");
-
-const radioNameArr = ["driverType"]
+const textInputArr = document.getElementsByClassName("text-input");
 
 const changeWidthBatch = (elementArr, targetWidth) => {
     for (let element of elementArr) {
@@ -18,18 +17,30 @@ const changeDropWidth = () => {
 const dropdownViewEvent = () => {
     // UNHIDE DROPDOWN WHEN FORM BUTTON CLICKED
     for (let i = 0; i < selectBtnArr.length; i++) {
+        const dropdown = selectDropArr[i];
+        const arrowImg = selectBtnArr[i].children[1];
+
+        const rotateImg = () => {
+
+        }
+
         selectBtnArr[i].addEventListener("click", () => {
-            const dropdown = selectDropArr[i];
             if (dropdown.classList.contains("active")) {
                 dropdown.classList.remove("active");
+                arrowImg.classList.remove("active");
             } else {
                 dropdown.classList.add("active");
+                arrowImg.classList.add("active");
             }
+            rotateImg()
         })
-    }
 
-    for (let i = 0; i < selectDropArr.length; i++) {
-        const element = selectDropArr[i].children;
+        selectBtnArr[i].addEventListener("focusout", () => {
+            setTimeout(() => {
+                dropdown.classList.remove("active");
+                arrowImg.classList.remove("active");
+            }, 120);
+        })
     }
 }
 
@@ -40,13 +51,42 @@ const dropdownClickEvent = () => {
             const listItem = list[j];
             listItem.addEventListener("click", () => {
                 const input = listItem.children[0]
-                const text = listItem.children[1].innerHTML;
+                let text = listItem.children[1].innerHTML;
+                if (input.name === "pickupTime") text += " WIB";
                 selectBtnArr[i].children[0].innerHTML = text;
                 input.checked = true;
 
                 selectDropArr[i].classList.remove("active");
             })
         }
+    }
+}
+
+const selectInputHoverEvent = () => {
+    for (let i = 0; i < selectInputArr.children.length; i++) {
+        const element = selectInputArr.children[i];
+
+    }
+}
+
+const textInputActiveEvent = () => {
+    for (let i = 0; i < textInputArr.length; i++) {
+        const input = textInputArr[i];
+        const inputContainer = input.children[1]
+        const inputBox = inputContainer.children[0];
+
+        inputBox.addEventListener("click", () => {
+            if (inputContainer.classList.contains("active")) {
+                inputContainer.classList.remove("active")
+            } else {
+                inputContainer.classList.add("active")
+            }
+        })
+        inputBox.addEventListener("focusout", () => {
+            setTimeout(() => {
+                inputContainer.classList.remove("active");
+            }, 120);
+        })
     }
 }
 
@@ -58,18 +98,7 @@ const start = () => {
     changeDropWidth();
     dropdownViewEvent();
     dropdownClickEvent();
+    textInputActiveEvent();
 }
 
 start()
-
-
-
-
-
-/* console.log(selectInputArr[0].children);
-let selectedInputNum = null;
-
-
-
-const radio1 = document.getElementsByName("driverType")
-console.log(radio1); */
